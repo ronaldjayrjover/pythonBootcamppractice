13032f4299a1ed2bf138a91d897a369ecb05f9a4
ronaldjayrjover <ronaldjover@gmail.com>
2019-01-28 01:02:53 -0800

    lezzzdoit

---
+++ courseIntro/whyPython3.py
@@ -0,0 +1,13 @@
+'''python 3 is not backwards compatible
+any libraries made with python 2 may/may not work
+
+
+
+excersises - they aren't distributed everly across the course
+'''
+
+def speak_pig():
+    return 'oink'
+
+print(speak_pig())
+

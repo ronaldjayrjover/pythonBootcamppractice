01fb12083392b912b9c33fd38e3aa77cd88a5a13
ronaldjayrjover <ronaldjover@gmail.com>
2019-02-04 01:06:15 -0800

    newer addon

--- numbers_operators_comments/numbersOperatorsComments.py
+++ numbers_operators_comments/numbersOperatorsComments.py
@@ -4,10 +4,11 @@
 
 print(type(9))
 print(type(1.222))
-print(1/3) #division always return quotes
+print(type(1/3)) #division always return quotes
 
 '''other math operators
 **  exponential 
 % modulo
 //integer division'''
 
+print(type(1+4.0))

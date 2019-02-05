420bfa3405228886a42e922ccab514ff0c0e3e08
ronaldjayrjover <ronaldjover@gmail.com>
2019-02-04 19:36:58 -0800

    bools and conditions

---
+++ boolean_conditionalLogic/booleans_and_conditionalLogic.py
@@ -0,0 +1,21 @@
+#### Getting Input
+#name = input("Enter your name here: ")
+#print("you said" + name)
+
+#### Conditional Statements
+
+""" if some condition is True:
+      do something
+    elif some other condition is True:
+      do something
+    else:
+      do something
+"""
+bname = input("Enter name here: ")
+if bname == "Jay":
+    print("test1")
+elif bname == "Ron":
+    print("test2")
+else:
+    print("test")
+

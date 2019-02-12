286e4f652fb3fe2f5494968e27ea3da82e4aca7e
ronaldjayrjover <ronaldjover@gmail.com>
2019-02-12 06:07:10 -0800

    additional looping exercise

---
+++ looping_in_python/repeaterExercise.py
@@ -0,0 +1,6 @@
+input = input("How many times do I have to tell you: ")
+input = int(input)
+
+for time in range(input):
+    print(f"{time+1} clean") ### the +1 is just to have it started on 1 and not start the time on 0
+
---
+++ looping_in_python/unluckyNumber.py
@@ -0,0 +1,14 @@
+'''
+loop through number 1-20
+for 4 and 13, print "x is unlucky"
+for even numbers, print "x is even"
+for odd numbers, print "x is odd"
+'''
+
+for x in range(1,21):
+    if x == 4 or x == 13:
+        print(f"{x} is unlucky")
+    elif x % 2 == 0: ### modulo(%) means if there is no remainder then its even
+        print(f"{x} is even")
+    else:
+        print(f"{x} is odd")

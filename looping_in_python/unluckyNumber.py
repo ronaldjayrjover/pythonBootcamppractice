f709e456d5595822b6ae6241b1db6c9828f06267
ronaldjayrjover <ronaldjover@gmail.com>
2019-02-12 06:10:31 -0800

    refactor the print commands

--- looping_in_python/unluckyNumber.py
+++ looping_in_python/unluckyNumber.py
@@ -12,3 +12,13 @@ for x in range(1,21):
         print(f"{x} is even")
     else:
         print(f"{x} is odd")
+
+###refactor/simplified below
+for x in range(1,21):
+    if x == 4 or x == 13:
+        state = "unlucky"
+    elif x % 2 == 0: ### modulo(%) means if there is no remainder then its even
+        state = "even"
+    else:
+        state = "odd"
+    print(f"{x} is {state}")
